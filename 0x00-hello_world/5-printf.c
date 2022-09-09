73145f58c0540713d5d25e99bb9f65490e495743
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-09 05:25:09 -0700

    real deal

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,7 @@
 
 /*
  * main - print the string in the printf function
+ *
  * Description: using the main function
  * this program prints with proper grammer, but the outcome is a piece of art,
  * Return: 0
