9f0cef80a8cb21a6a7270cf1ddc187a759af81b5
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-09 04:43:47 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /*
-*main type is void
+* main - print the string using the put function
 *
-* write a code to print programming is like bulding a multilingual puzzle.
-*
+* Description: using the main function 
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {        
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-	
