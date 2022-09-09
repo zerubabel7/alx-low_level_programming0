e9696c02e0d2b7898e73877b88c2c3aff5b8a33d
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-09 05:28:48 -0700

    real deal

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- * main - print the string in the printf function
+ *main - print the string in the printf function
  *
- * Description: using the main function
- * this program prints with proper grammer, but the outcome is a piece of art,
- * Return: 0
+ *Description: using the main function
+ *this program prints "with proper grammer, but the outcome is a piece of art,"
+ *Return: 0
  */
 int main(void)
 {
