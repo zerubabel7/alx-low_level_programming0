b9bd6c8c5b7f07aa42e749b5f239b26d47ba035f
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-09 04:59:32 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
 * main - print the string in the put function.
-* Description: using the main function 
+* Description:using the main function 
 * this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+* Return:0
 */
 int main(void)
 {        
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
