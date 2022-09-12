2b71e8ab7e6f1b6df5a61dcc19f6bcc3a16f8089
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-12 04:03:59 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - write a program that prints all possible combinations of single numbers.
+ * Description: using the main function
+ * this program prints all possible combinations of single numbers
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+	if (c != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
