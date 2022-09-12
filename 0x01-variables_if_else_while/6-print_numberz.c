52e56f917697b66e993f1292cac6a7185be52ec5
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-12 03:36:38 -0700

    new num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                                                                                                            
+
+/**                                                                                                                           
+ *
+ * main - Write a program that prints all single digit numbers of base 10                                                 
+ * Description: using the main function                                                                                       
+ * this program prints all single digit numbers                                                                               
+ * Return: 0                                                                                                                  
+ */                                                                                                                           
+
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
