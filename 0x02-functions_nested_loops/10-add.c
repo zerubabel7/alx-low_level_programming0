f8a30e77542fbd53e90a7c960caf00ba7e6bceb5
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-13 05:52:31 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * prints -> result of the sum
+ * @a and @ b: used as intigers
+ * Return: sum of the two
+ */
+
+int add(int, int a, b)
+{
+	add(int a, int b)
+	{
+		return a + b;
+	}
+}
