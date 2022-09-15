c6463745055cf3b9e8564d983b62cfb24e40d57d
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-15 06:34:04 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,14 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y + '0');
+			if (y > 9)
+			{
+				putchar((y / 10) + '0');
+			}
+			putchar((y % 10) + '0');
 		}
-		_putchar('\n')
+		putchar(10);
 	}
 }
