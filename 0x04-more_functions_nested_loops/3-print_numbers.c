fb9e6a9117d44a446ec88e3f0b64cc69962e65f3
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-15 06:42:23 -0700

    number printer

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,17 @@
+#include<stdio.h>
 #include "main.h"
 
 /**
  * print_numbers -> print numbers
+ * Return: nothing
  */
 void print_numbers(void)
 {
 	int x;
 
-	for (x = 0; x < 10; x++)
-		_putchar(x + '0')
-	_putchar('\n');
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+        putchar(10);
 }
