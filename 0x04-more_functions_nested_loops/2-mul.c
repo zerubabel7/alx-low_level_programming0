6af069a70e35f2368ddfd7c094b078e66daf7d1f
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-15 04:51:10 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - function that checks multiples of numbers
+ * @a: intiger parameter
+ * @b: second intiger parameter
+ * Return: success and intiger
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
