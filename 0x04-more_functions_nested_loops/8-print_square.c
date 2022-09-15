339e5d2884386894c22eb4ff46245836298da876
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-15 06:46:01 -0700

    hot fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,7 @@ void print_square(int size)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
-			for(inc2 < 0; inc2 < (size - 1); inc2++)
+			for(inc2 = 0; inc2 < (size - 1); inc2++)
 			{
 				putchar('#');
 			}
