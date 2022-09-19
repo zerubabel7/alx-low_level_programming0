904c4e2f8bb6710da70da2822ec32b96e44b7edb
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-19 05:46:19 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -> swaps two intiger values
+ * @a: first intiger
+ * @b: second intiger
+ * Return: returns nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp
+}
+
