0e353c677474cc77e6d85449b005e96bc34ab815
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-19 07:02:28 -0700

    reset 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98.c - resets the value to 98
+ * reset_to_98.c -> resets the value to 98
  * @n: parameter
- * description: resets to 98
+ * Description: using the main function
+ * this program prints everything before 98
  *
  * Return: nothing
  */
