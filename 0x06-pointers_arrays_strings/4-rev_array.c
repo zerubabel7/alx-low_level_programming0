8b6dd6ee0881516f3734adf8cd4a9f07958ef86c
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-09-22 07:08:21 -0700

    hotfix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 	for (index = n -1; index > n / 2; index --)
 	{
 		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[inddex];
+		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
 }
